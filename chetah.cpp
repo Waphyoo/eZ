880b439eea6367ccdc5cd03a995030d30a60f8f0
Waphyoo <110945410+Waphyoo@users.noreply.github.com>
2022-12-15 18:23:16 +0700

    okok

--- chetah.cpp
+++ chetah.cpp
@@ -4,21 +4,20 @@ using namespace std;
 
 int cheetah(int money){
     int bot = money/10;
-    int i=bot-1;
+    int i=bot;
     while(bot>2){
         bot=bot-2;
         i++;
 
     }
-    if(i==bot-1){
-        return bot;
-    }else{
-    return i+bot;
-    }
+    
+    return i;
+    
 } 
 
+
 int main(){
-    cout<<cheetah(100);
+    cout<<cheetah(101);
 
 
 }
